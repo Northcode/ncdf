dda1ea5afab80121b9b59e374056edbf9a52bfb4
Andreas Larsen <northcode.no@gmail.com>
2013-11-27 22:34:02 +0100

    stuff

--- ncdf/MenuManager.cs
+++ ncdf/MenuManager.cs
@@ -8,11 +8,29 @@ namespace ncdf
 {
     interface IMenu
     {
+        string Name;
+
         void Output();
     }
 
     class MenuManager
     {
+        static IMenu[] menus = new IMenu[0];
+        static int selectedMenu;
 
+        static void ShowMenu(string Name)
+        {
+            for (selectedMenu = 0; menus[selectedMenu] != null && menus[selectedMenu].Name != Name; selectedMenu++) ;
+        }
+
+        static void ShowMenu(int n)
+        {
+            selectedMenu = n;
+        }
+
+        static void Output()
+        {
+            menus[selectedMenu].Output();
+        }
     }
 }
